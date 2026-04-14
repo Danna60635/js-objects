4815e12c77c494793db8efb51d14a2953ebb17cf
Danna60635 <1301235924@s.tusd1.org>
2026-04-14 21:57:42 +0000

    Done

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 console.log("Script started")
 
 // TODO: Test working with objects here
+    let person = { name: "John", age: 30};
+    console.log(person.name);
+    person.name = "George harrison";
+    console.log(person.name);
+
+    person.favColor = "Yellow";
+    console.log(person);
 
 
-
-function processForm() {
+function processForm(event) {
     console.log("click");
     // TODO: prevent this function from reloading the page when the form is submitted
-
+    event.preventDefault();
     // TODO: Create a newUser object that has all the user's info from the form
+    let newUser = {
+        name: document.getElementbyId("name");
+        email: document.getElementById("email");
+        birthdate: document.getElementById("birthdate");
+        favColor: document.getElementById("favcolor");
+    }
+
+    console.log(newUser);
+
 
     // TODO: Call the addUser function and pass the newUser object as a parameter
+    addUser(newUser);
 }
 
 function addUser(user) {
     // Create a div to hold the user's info
     let div = document.createElement("div");
     // TODO: Set the background color of the div to the user's favorite color
-
+    
     // Create a heading for the user's name
     let name = document.createElement("h2");
     // TODO: Set the text using the object data
+    name.innerText = user.name;
     div.appendChild(name); // Add the heading to the div
 
     // Create a paragraph for the user's email
